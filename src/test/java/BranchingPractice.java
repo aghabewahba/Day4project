0a75aaa4e0ca479705f7cc8c6ee1632897a978cc
aghabewahba <aghaby69@hotmail.com>
2021-05-26 08:56:17 -0400

    shred on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,11 @@
 public class BranchingPractice {
     public static void main(String[] args) {
 
+        //creat new project
+        //enabled version control
+        //added .ignore with .idea , target folder and .iml file
+        //made initial commit by keeping track of all files
+        //share project on github
+
     }
 }
